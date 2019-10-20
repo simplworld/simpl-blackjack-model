80a0c66fca522033da76d9734e225a42eef2dd6d
Frank Wiles <frank@revsys.com>
2019-10-20 10:01:04 -0500

    Working on component load

--- game/games.py
+++ game/games.py
@@ -1,7 +1,9 @@
+import uuid
+
 from modelservice.games import Period, Game
 from modelservice.games import subscribe
-
-from .runmodel import step_scenario, save_decision
+from modelservice.simpl import games_client
+from .model import Model
 
 
 class BlackjackPeriod(Period):
@@ -15,13 +17,92 @@ class BlackjackPeriod(Period):
         # Call will prefix the ROOT_TOPIC
         # "world.simpl.sims.blackjack.model.period.1.submit_decision"
 
-        for k in kwargs:
-            self.session.log.info("submit_decision: Key: {}".format(k))
+        # Log our decision
+        self.session.log.info("submit_decision: '{}'".format(action))
 
-        await save_decision(self.pk, action)
+        # Save our decision
+        async with games_client as api_session:
+            await api_session.decisions.create(
+                {
+                    "period": self.pk,
+                    "name": action,
+                    "data": {"action": action},
+                    "role": None,
+                }
+            )
         self.session.log.info("submit_decision: saved decision")
 
-        await step_scenario(self.scenario.pk, action)
+        scenario_id = self.scenario.pk
+
+        # Get last period
+        periods = self.scenario.periods.all()
+
+        async with games_client as api_session:
+            periods = await api_session.periods.filter(
+                scenario=scenario_id, ordering="order"
+            )
+            period_count = len(periods)
+            period = periods[period_count - 1]
+
+            print("Period")
+            print(period)
+            if action is None:
+                action = "deal"
+
+            data = {}
+            if period_count > 1:
+                prev_period = periods[period_count - 2]
+
+                # if prev_period.results.all():
+                #    data = prev_period.results.all()[0].json.get("data")
+                #    print("Period Data")
+                #    print(data)
+                prev_period_results = await api_session.results.filter(
+                    period=prev_period.pk
+                )
+                if len(prev_period_results) > 0:
+                    data = prev_period_results[0].data["data"]
+
+            model = Model()
+            data = model.step(action, data)
+            data = {"data": data}
+
+            if not action == "new":
+                # step model
+                result = await api_session.results.create(
+                    {"period": period.pk, "name": "results", "data": data, "role": None}
+                )
+
+                # prepare for next step by adding a new period
+                next_period_order = period.order + 1
+                next_period = await api_session.periods.create(
+                    {"scenario": scenario_id, "order": next_period_order, "data": {}}
+                )
+
+            if action == "new":
+                old_scenario = await api_session.scenarios.filter(id=scenario_id)
+                runuser = old_scenario[0].runuser
+                new_scenario = await api_session.scenarios.create(
+                    {"runuser": runuser, "name": "Scenario {}".format(uuid.uuid4())}
+                )
+
+                new_period_for_new_scenario = await api_session.periods.create(
+                    {"scenario": new_scenario.id, "order": 1, "data": {}}
+                )
+
+                result = await api_session.results.create(
+                    {
+                        "period": new_period_for_new_scenario.id,
+                        "name": "results",
+                        "data": data,
+                        "role": None,
+                    }
+                )
+
+                next_period = await api_session.periods.create(
+                    {"scenario": new_scenario.id, "order": 2, "data": {}}
+                )
+
         self.session.log.info("submit_decision: stepped scenario")
 
 
--- game/model.py
+++ game/model.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Model(object):
     """
     The model modifies the game data based on 3 action:
@@ -18,77 +19,71 @@ class Model(object):
 
     def __init__(self):
         self.data = {
-            'deck': [],
-            'player_cards': [],
-            'dealer_cards': [],
-            'player_score': 0,
-            'dealer_score': 0,
-            'player_busted': False,
-            'dealer_busted': False,
-            'push': False,
-            'player_done': False
+            "deck": [],
+            "player_cards": [],
+            "dealer_cards": [],
+            "player_score": 0,
+            "dealer_score": 0,
+            "player_busted": False,
+            "dealer_busted": False,
+            "push": False,
+            "player_done": False,
         }
 
     def createDeck(self):
         deck = []
-        ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
-        suits = ['H', 'S', 'C', 'D']
+        ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+        suits = ["H", "S", "C", "D"]
 
         for rank in ranks:
             for suit in suits:
-                deck.append({
-                        'rank': rank,
-                        'suit': suit
-                    })
+                deck.append({"rank": rank, "suit": suit})
 
         random.shuffle(deck)
         return deck
 
     def calcRankScore(self, rank):
-        score = {
-            'hard_total': 0,
-            'soft_total': 0
-        }
-        if (rank == 'A'):
-            score['hard_total'] += 1
-            score['soft_total'] += 1 if ((score['soft_total'] + 11) > 21) else 11
+        score = {"hard_total": 0, "soft_total": 0}
+        if rank == "A":
+            score["hard_total"] += 1
+            score["soft_total"] += 1 if ((score["soft_total"] + 11) > 21) else 11
         elif isinstance(rank, str):
-            score['hard_total'] += 10
-            score['soft_total'] += 10
+            score["hard_total"] += 10
+            score["soft_total"] += 10
         else:
-            score['hard_total'] += rank
-            score['soft_total'] += rank
+            score["hard_total"] += rank
+            score["soft_total"] += rank
         return score
 
     def calcTotal(self, score):
-        if score['hard_total'] == 21 or score['soft_total'] == 21:
+        if score["hard_total"] == 21 or score["soft_total"] == 21:
             return 21
-        elif score['soft_total'] > 21:
-            return score['hard_total']
-        return score['soft_total']
+        elif score["soft_total"] > 21:
+            return score["hard_total"]
+        return score["soft_total"]
 
     def calcHandScore(self, cards):
         score = 0
         for card in cards:
-            score += self.calcTotal(self.calcRankScore(card['rank']))
+            score += self.calcTotal(self.calcRankScore(card["rank"]))
         return score
 
     def isBusted(self, score):
         return score > 21
 
     def dealerTurn(self, data):
-        data['player_done'] = True
-        while data['dealer_score'] < 21:
-            card = data.get('deck', []).pop()
-            data['dealer_cards'].append(card)
-            data['dealer_score'] = self.calcHandScore(data['dealer_cards'])
-        data['dealer_busted'] = self.isBusted(data['dealer_score'])
-        if data['dealer_score'] < data['player_score']:
-            data['player_busted'] = True
-        if data['dealer_score'] > data['player_score']:
-            data['dealer_busted'] = True
-        if data['dealer_score'] == data['player_score']:
-            data['push'] = True
+        data["player_done"] = True
+        while data["dealer_score"] < 21:
+            card = data.get("deck", []).pop()
+            data["dealer_cards"].append(card)
+            data["dealer_score"] = self.calcHandScore(data["dealer_cards"])
+        data["dealer_busted"] = self.isBusted(data["dealer_score"])
+        if data["dealer_score"] < data["player_score"]:
+            data["player_busted"] = True
+        if data["dealer_score"] > data["player_score"]:
+            data["dealer_busted"] = True
+        if data["dealer_score"] == data["player_score"]:
+            data["push"] = True
         return data
 
     def step(self, action, data=None):
@@ -98,33 +93,36 @@ class Model(object):
             data - the game data (cards, scores etc)
         Returns new total
         """
+        print("Stepping!")
+        print(action)
+        print(data)
         if data is None:
             data = {}
 
-        if action == 'new':
+        if action == "new":
             return self.data
 
-        if action == 'deal':
+        if action == "deal":
             deck = self.createDeck()
             deal_data = self.data.copy()
-            deal_data['deck'] = deck
-            deal_data['player_cards'].append(deck.pop())
-            deal_data['dealer_cards'].append(deck.pop())
-            deal_data['player_cards'].append(deck.pop())
-            deal_data['player_score'] = self.calcHandScore(deal_data['player_cards'])
-            deal_data['dealer_score'] = self.calcHandScore(deal_data['dealer_cards'])
+            deal_data["deck"] = deck
+            deal_data["player_cards"].append(deck.pop())
+            deal_data["dealer_cards"].append(deck.pop())
+            deal_data["player_cards"].append(deck.pop())
+            deal_data["player_score"] = self.calcHandScore(deal_data["player_cards"])
+            deal_data["dealer_score"] = self.calcHandScore(deal_data["dealer_cards"])
             return deal_data
 
-        elif action == 'hit':
-            card = data.get('deck', []).pop()
-            data['player_cards'].append(card)
-            data['player_score'] = self.calcHandScore(data['player_cards'])
-            data['player_busted'] = self.isBusted(data['player_score'])
-            if data['player_busted']:
-                data['player_done'] = True
-            if data['player_score'] == 21:
+        elif action == "hit":
+            card = data.get("deck", []).pop()
+            data["player_cards"].append(card)
+            data["player_score"] = self.calcHandScore(data["player_cards"])
+            data["player_busted"] = self.isBusted(data["player_score"])
+            if data["player_busted"]:
+                data["player_done"] = True
+            if data["player_score"] == 21:
                 data = self.dealerTurn(data)
             return data
-        elif action == 'stand':
+        elif action == "stand":
             data = self.dealerTurn(data)
             return data
