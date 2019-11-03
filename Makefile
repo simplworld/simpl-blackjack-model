3f3099458901d9ceba353e6934a4117bed2ed3da
Frank Wiles <frank@revsys.com>
2019-11-03 15:15:44 -0600

    Fix double dealing issues

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-.PHONEY: shell
+.PHONEY: shell rebuild
+
+rebuild:
+	docker-compose rm -f model.backend
+	docker-compose build --force-rm model.backend
 
 shell:
 	docker-compose run --rm model.backend bash
--- game/games.py
+++ game/games.py
@@ -1,13 +1,35 @@
-import uuid
+import asyncio
 
-from modelservice.games import Period, RunUser, Game
-from modelservice.games import subscribe, register
+from modelservice.games import Scenario, Game
+from modelservice.games import register
 from modelservice.simpl import games_client
 from .model import Model
 
 
-class BlackjackPeriod(Period):
-    @subscribe
+class BlackjackScenario(Scenario):
+    @register
+    async def deal_new_game(self, **kwargs):
+        """
+        Deal a new game to the user
+        """
+        self.session.log.info("deal_new_game: {}".format(self.pk))
+
+        async with games_client as api_session:
+            # Delete all existing Periods
+            await api_session.bulk.periods.delete(scenario=self.pk)
+            self.session.log.info("Deleted bulk periods!!!!")
+
+            await asyncio.sleep(0.5)
+            self.session.log.info("Woke from sleep!!!!")
+
+            # Create a new initial period
+            new_period = await api_session.periods.create(
+                {"scenario": self.pk, "order": 1, "data": {}}
+            )
+
+            return new_period.id
+
+    @register
     async def submit_decision(self, action, **kwargs):
         """
         Receives the operand played and stores as a ``Decision`` then
@@ -15,29 +37,36 @@ class BlackjackPeriod(Period):
         scenario in preparation for the next decision.
         """
         # Call will prefix the ROOT_TOPIC
-        # "world.simpl.sims.blackjack.model.period.1.submit_decision"
+        # "world.simpl.sims.blackjack.model.scenario.1.submit_decision"
 
         # Log our decision
         self.session.log.info("submit_decision: '{}'".format(action))
+
+        scenario_id = self.pk
+
+        # Get last period
+        async with games_client as api_session:
+            periods = await api_session.periods.filter(
+                scenario=scenario_id, ordering="order"
+            )
+            period_count = len(periods)
+            last_period = periods[period_count - 1]
+
+        # periods = self.periods.all()
+        # periods = list(periods)
+        # last_period = periods[-1]
 
         # Save our decision
         async with games_client as api_session:
             await api_session.decisions.create(
                 {
-                    "period": self.pk,
+                    "period": last_period.pk,
                     "name": action,
                     "data": {"action": action},
                     "role": None,
                 }
             )
         self.session.log.info("submit_decision: saved decision")
-
-        scenario_id = self.scenario.pk
-
-        # Get last period
-        periods = self.scenario.periods.all()
-        print("Periods")
-        print(periods)
 
         async with games_client as api_session:
             periods = await api_session.periods.filter(
@@ -56,10 +85,6 @@ class BlackjackPeriod(Period):
             if period_count > 1:
                 prev_period = periods[period_count - 2]
 
-                # if prev_period.results.all():
-                #    data = prev_period.results.all()[0].json.get("data")
-                #    print("Period Data")
-                #    print(data)
                 prev_period_results = await api_session.results.filter(
                     period=prev_period.pk
                 )
@@ -70,43 +95,18 @@ class BlackjackPeriod(Period):
             data = model.step(action, data)
             data = {"data": data}
 
-            if not action == "new":
-                # step model
-                result = await api_session.results.create(
-                    {"period": period.pk, "name": "results", "data": data, "role": None}
-                )
+            # step model
+            result = await api_session.results.create(
+                {"period": period.pk, "name": "results", "data": data, "role": None}
+            )
 
-                # prepare for next step by adding a new period
-                next_period_order = period.order + 1
-                next_period = await api_session.periods.create(
-                    {"scenario": scenario_id, "order": next_period_order, "data": {}}
-                )
-
-            if action == "new":
-                old_scenario = await api_session.scenarios.filter(id=scenario_id)
-                runuser = old_scenario[0].runuser
-                new_scenario = await api_session.scenarios.create(
-                    {"runuser": runuser, "name": "Scenario {}".format(uuid.uuid4())}
-                )
-
-                new_period_for_new_scenario = await api_session.periods.create(
-                    {"scenario": new_scenario.id, "order": 1, "data": {}}
-                )
-
-                result = await api_session.results.create(
-                    {
-                        "period": new_period_for_new_scenario.id,
-                        "name": "results",
-                        "data": data,
-                        "role": None,
-                    }
-                )
-
-                next_period = await api_session.periods.create(
-                    {"scenario": new_scenario.id, "order": 2, "data": {}}
-                )
+            # prepare for next step by adding a new period
+            next_period_order = period.order + 1
+            next_period = await api_session.periods.create(
+                {"scenario": scenario_id, "order": next_period_order, "data": {}}
+            )
 
         self.session.log.info("submit_decision: stepped scenario")
 
 
-Game.register("blackjack", [BlackjackPeriod])
+Game.register("blackjack", [BlackjackScenario])
